eca4dbc0ead5cd172412305fa4c523b5cc9faad7
Martin <131613019+martinmorichaud@users.noreply.github.com>
2025-01-23 09:43:25 +0100

    Update vues.sql

--- vues.sql
+++ vues.sql
@@ -1,10 +1,27 @@
-CREATE OR REPLACE VIEW Propositions_Recentes AS
-SELECT *
-FROM Proposition
-ORDER BY dateProp DESC;
+CREATE VIEW PropositionsRecentes AS
+SELECT P.idProposition, P.titreProposition, P.descProposition, P.popularite
+FROM Proposition P
+ORDER BY P.idProposition DESC;
 
+CREATE OR REPLACE VIEW PropositionsPopulaires AS
+SELECT P.idProposition, P.titreProposition, P.descProposition, P.popularite
+FROM Proposition P
+ORDER BY P.popularite DESC;
 
-CREATE OR REPLACE VIEW Propositions_Populaires AS
-SELECT *
-FROM Proposition
-ORDER BY popularite DESC;
+CREATE OR REPLACE VIEW GroupesUtilisateur AS
+SELECT G.idGroupe, G.nomGroupe, M.idMembre
+FROM Groupe G
+INNER JOIN Fait_partie_de FPD ON G.idGroupe = FPD.idGroupe
+INNER JOIN Membre M ON FPD.idMembre = M.idMembre;
+
+CREATE VIEW PropositionsUtilisateur AS
+SELECT P.idProposition, P.titreProposition, P.descProposition, P.popularite, M.idMembre
+FROM Proposition P
+INNER JOIN Membre M ON P.idProposition = M.idProposition;
+
+CREATE VIEW MembresGroupe AS
+SELECT G.idGroupe, G.nomGroupe, M.idMembre, M.roleMembre
+FROM Groupe G
+INNER JOIN Fait_partie_de FPD ON G.idGroupe = FPD.idGroupe
+INNER JOIN Membre M ON FPD.idMembre = M.idMembre;
+
