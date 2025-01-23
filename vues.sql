856f169d6ce5c15be6d99d1137d5b6969b62483a
Martin <131613019+martinmorichaud@users.noreply.github.com>
2025-01-23 10:02:05 +0100

    Update vues.sql

--- vues.sql
+++ vues.sql
@@ -9,19 +9,21 @@ FROM Proposition P
 ORDER BY P.popularite DESC;
 
 CREATE OR REPLACE VIEW GroupesUtilisateur AS
-SELECT G.idGroupe, G.nomGroupe, M.idMembre
-FROM Groupe G
-INNER JOIN Fait_partie_de FPD ON G.idGroupe = FPD.idGroupe
-INNER JOIN Membre M ON FPD.idMembre = M.idMembre;
+SELECT FPD.loginInter, G.idGroupe, G.nomGroupe, R.nomRole 
+FROM Fait_partie_de FPD 
+INNER JOIN Groupe G ON FPD.idGroupe = G.idGroupe 
+INNER JOIN Role R ON FPD.idRole = R.idRole;
 
 CREATE OR REPLACE VIEW PropositionsUtilisateur AS
-SELECT P.idProposition, P.titreProposition, P.descProposition, P.popularite, M.idMembre
-FROM Proposition P
-INNER JOIN Membre M ON P.idProposition = M.idProposition;
+SELECT PR.loginInter, P.idProposition, P.titreProposition, P.descProposition, P.popularite, P.dateProp 
+FROM Propose PR 
+INNER JOIN Proposition P ON PR.idProposition = P.idProposition;
 
 CREATE OR REPLACE VIEW MembresGroupe AS
-SELECT G.idGroupe, G.nomGroupe, M.idMembre, M.roleMembre
-FROM Groupe G
-INNER JOIN Fait_partie_de FPD ON G.idGroupe = FPD.idGroupe
-INNER JOIN Membre M ON FPD.idMembre = M.idMembre;
+SELECT G.idGroupe, G.nomGroupe, I.loginInter, I.nomInter, I.prenomInter, R.nomRole 
+FROM Fait_partie_de FPD 
+INNER JOIN Groupe G ON FPD.idGroupe = G.idGroupe 
+INNER JOIN Internaute I ON FPD.loginInter = I.loginInter 
+INNER JOIN Role R ON FPD.idRole = R.idRole;
+
 
