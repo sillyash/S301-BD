f3995662f5ddeee8353c47c5f51968691ebe66f4
Martin <131613019+martinmorichaud@users.noreply.github.com>
2025-01-24 09:04:15 +0100

    Update vues.sql

--- vues.sql
+++ vues.sql
@@ -3,10 +3,19 @@ SELECT P.idProposition, P.titreProposition, P.descProposition, P.popularite, P.v
 FROM Proposition P
 ORDER BY P.idProposition DESC;
 
-CREATE OR REPLACE VIEW PropositionsPopulaires AS
+/*CREATE OR REPLACE VIEW PropositionsPopulaires AS
 SELECT P.idProposition, P.titreProposition, P.descProposition, P.popularite, P.validee
 FROM Proposition P
 ORDER BY P.popularite DESC;
+*/
+
+CREATE OR REPLACE VIEW PropositionsPopulaires AS
+SELECT g.nomGroupe, t.nomTheme, p.titreProposition, p.descProposition, p.popularite
+FROM Groupe g
+JOIN Theme t ON g.idGroupe = t.idGroupe
+JOIN A_pour_theme apt ON t.idTheme = apt.idTheme
+JOIN Proposition p ON apt.idProposition = p.idProposition
+ORDER BY p.popularite DESC;
 
 CREATE OR REPLACE VIEW GroupesUtilisateur AS
 SELECT FPD.loginInter, G.idGroupe, G.nomGroupe, R.nomRole 
@@ -25,5 +34,21 @@ FROM Fait_partie_de FPD
 INNER JOIN Groupe G ON FPD.idGroupe = G.idGroupe 
 INNER JOIN Internaute I ON FPD.loginInter = I.loginInter 
 INNER JOIN Role R ON FPD.idRole = R.idRole;
+
+CREATE OR REPLACE VIEW VueBudgetsParThematique AS
+SELECT g.nomGroupe, t.nomTheme, b.limiteBudgetGlobal, SUM(p.popularite) AS PopulariteTotale
+FROM Groupe g
+INNER JOIN Theme t ON g.idGroupe = t.idGroupe
+INNER JOIN Proposition p ON p.idProposition = (SELECT idProposition FROM A_pour_theme WHERE idTheme = t.idTheme)
+INNER JOIN Budget b ON p.idBudget = b.idBudget
+GROUP BY g.nomGroupe, t.nomTheme, b.limiteBudgetGlobal;
+
+CREATE OR REPLACE VIEW VuePropositionsValidees AS
+SELECT g.nomGroupe, t.nomTheme, p.titreProposition, p.descProposition, p.popularite, p.validee
+FROM Groupe g
+INNER JOIN Theme t ON g.idGroupe = t.idGroupe
+INNER JOIN A_pour_theme apt ON t.idTheme = apt.idTheme
+INNER JOIN Proposition p ON apt.idProposition = p.idProposition
+WHERE p.validee = TRUE;
 
 
