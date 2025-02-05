f84bde6fd2fa03522df41aa68f3a685e4ef9cf5d
SassyAsh <swannmerienne@gmail.com>
2025-02-05 15:49:39 +0100

    feat: modify PropositionsGroupe view to use LEFT JOINs and adjust GROUP BY and ORDER BY clauses

--- vues.sql
+++ vues.sql
@@ -75,25 +75,10 @@ JOIN A_pour_theme apt ON p.idProposition = apt.idProposition
 JOIN Theme t ON apt.idTheme = t.idTheme
 JOIN Budget b ON p.idBudget = b.idBudget
 JOIN Groupe g ON b.idGroupe = g.idGroupe
-JOIN Scrutin s ON p.idProposition = s.idProposition
-JOIN Vote v ON s.idScrutin = v.idScrutin
-GROUP BY
-    p.idProposition,
-    p.titreProposition,
-    p.descProposition,
-    p.popularite,
-    p.idBudget,
-    t.nomTheme,
-    g.idGroupe,
-    g.nomGroupe,
-    s.idScrutin,
-    s.natureScrutin,
-    s.dureeScrutin,
-    s.dureeDiscussion,
-    s.resultatScrutin,
-    b.limiteBudgetGlobal,
-    b.titreBudget
-ORDER BY p.popularite DESC;
+LEFT JOIN Scrutin s ON p.idProposition = s.idProposition
+LEFT JOIN Vote v ON s.idScrutin = v.idScrutin
+GROUP BY p.idProposition
+ORDER BY p.idProposition DESC;
 
 CREATE OR REPLACE VIEW PropositionsGroupe AS
 SELECT 
